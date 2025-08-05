744b7d9759aef04f059fea2e78eb17ec94af17cd
JoBEph <jordan.baroux31@gmail.com>
2025-08-05 13:24:30 +0200

    add function to determine if all boxes can be unlocked

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""function to determine if all boxes can be unlocked"""
+
+
+def canUnlockAll(boxes):
+    """Determine if all boxes can be unlocked"""
+    for n in range(len(boxes)):
+        if n == 0:
+            continue
+        if not boxes[n]:
+            return False
+        else:
+            for key in boxes[n]:
+                if key < len(boxes) and key != n:
+                    boxes[key].append(n)
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    stack = [0]
+
+    while stack:
+        current = stack.pop()
+        for key in boxes[current]:
+            if not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked)
