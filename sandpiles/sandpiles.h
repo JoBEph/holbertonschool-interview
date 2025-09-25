7c9cf05cc7e5fde18fef508fa10e2752eb973cb4
JoBEph <jordan.baroux31@gmail.com>
2025-09-25 10:09:15 +0200

    feat: add header file for sandpiles functions and prototypes

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+/* prototype */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void topple(int grid[3][3]);
+int stable(int grid[3][3]);
+
+#endif
